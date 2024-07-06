21bf4e2dd2ab622b03a98e158ba10ecc09ba3be4
Mustafa Ashraf Selim <122466340+Mustafa-Ashraf751@users.noreply.github.com>
2024-07-06 09:25:03 +0300

    Adding project files

---
+++ src/main/java/com/BlogApplication/Bloging/application/api/BlogingApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.BlogApplication.Bloging.application.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogingApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogingApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BlogApplication/Bloging/application/api/BlogingApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogApplication.Bloging.application.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogingApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
