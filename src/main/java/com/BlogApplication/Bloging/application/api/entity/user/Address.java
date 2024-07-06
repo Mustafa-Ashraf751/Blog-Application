a27f61788dd390ceae678aec23cf2a1d4c2c36c7
Mustafa Ashraf Selim <122466340+Mustafa-Ashraf751@users.noreply.github.com>
2024-07-06 11:04:55 +0300

    Creating Address claass

---
+++ src/main/java/com/BlogApplication/Bloging/application/api/entity/user/Address.java
@@ -0,0 +1,15 @@
+package com.BlogApplication.Bloging.application.api.entity.user;
+
+import java.time.LocalDateTime;
+
+public class Address {
+  private Long id;
+  private String street;
+  private String suite;
+  private String city;
+  private String zipcode;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+
+}
