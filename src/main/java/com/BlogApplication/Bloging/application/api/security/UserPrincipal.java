b25563ba3d2d5ef8f81cec8e787ecf143cc23c2b
Mustafa Ashraf Selim <122466340+Mustafa-Ashraf751@users.noreply.github.com>
2024-07-06 12:07:14 +0300

    Creating UserPrincipal class

---
+++ src/main/java/com/BlogApplication/Bloging/application/api/security/UserPrincipal.java
@@ -0,0 +1,90 @@
+package com.BlogApplication.Bloging.application.api.security;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+public class UserPrincipal implements UserDetails {
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String userName;
+
+  @JsonIgnore
+  private String email;
+  @JsonIgnore
+  private String password;
+
+  private Collection<? extends GrantedAuthority> authorities;
+
+  public UserPrincipal(Long id, String firstName, String lastName, String userName, String email,
+                       String password, Collection<? extends GrantedAuthority> authorities) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+    if(authorities == null){
+      this.authorities = null;
+    }else{
+      this.authorities = new ArrayList<>(authorities);
+    }
+
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getPassword() {
+    return "";
+  }
+
+  @Override
+  public String getUsername() {
+    return "";
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    UserPrincipal that = (UserPrincipal) o;
+    return Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+}
