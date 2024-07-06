9aa8198b6178b294f9d8c8cf55699b960910df80
Mustafa Ashraf Selim <122466340+Mustafa-Ashraf751@users.noreply.github.com>
2024-07-07 01:21:58 +0300

    Create Role class

---
+++ src/main/java/com/BlogApplication/Bloging/application/api/entity/role/Role.java
@@ -0,0 +1,25 @@
+package com.BlogApplication.Bloging.application.api.entity.role;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated
+  @NaturalId
+  private RoleName name;
+
+  public Role(RoleName name) {
+    this.name = name;
+  }
+}
