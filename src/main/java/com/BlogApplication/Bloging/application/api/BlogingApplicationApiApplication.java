a1df5bf2da1955ad2659b25723b59deab26102e0
Mustafa-Ashraf <mustafaashraf123@yahoo.com>
2024-07-15 07:14:40 +0300

    Add model mapper to my application

--- src/main/java/com/BlogApplication/Bloging/application/api/BlogingApplicationApiApplication.java
+++ src/main/java/com/BlogApplication/Bloging/application/api/BlogingApplicationApiApplication.java
@@ -1,10 +1,17 @@
 package com.BlogApplication.Bloging.application.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogingApplicationApiApplication {
+
+  @Bean
+  public ModelMapper createModel(){
+    return new ModelMapper();
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogingApplicationApiApplication.class, args);
