0ea6904359da39692269239d029312dd9593eea0
Mustafa-Ashraf <mustafaashraf123@yahoo.com>
2024-07-16 15:34:51 +0300

    Create UserIdentityAvailability to check the account availability

---
+++ src/main/java/com/BlogApplication/Bloging/application/api/DTO/UserIdentityAvailability.java
@@ -0,0 +1,14 @@
+package com.BlogApplication.Bloging.application.api.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserIdentityAvailability {
+
+  private Boolean available;
+
+}
